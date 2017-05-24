a764d8a37b93a7cdf6efb8953dbee6ca5f8e2cc3
joaoarthur <joaoarthur@joaoarthur.com.br>
2017-05-24 00:14:00 -0300

    API Rest Spring - Parte 3

--- src/main/java/com/rest/api/resource/ProdutoResource.java
+++ src/main/java/com/rest/api/resource/ProdutoResource.java
@@ -4,8 +4,10 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -32,9 +34,34 @@ public class ProdutoResource {
 		produto = produtos.save(produto);
 		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("{codigo}").buildAndExpand(produto.getCodigo()).toUri();
+				.path("/{codigo}").buildAndExpand(produto.getCodigo()).toUri();
 		
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{codigoBarras}", method = RequestMethod.GET)
+	public ResponseEntity<?> buscarPorCodigoBarras(@PathVariable("codigoBarras") 
+		String codigoBarras){
+		
+		Produto produto = produtos.findByCodigoBarras(codigoBarras);
+		
+		if (produto == null){
+			return ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body(produto);
+		
+	}
+	
+	@RequestMapping(value = "/{codigo}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> deletar(@PathVariable("codigo") Long codigo){
+		try{
+			produtos.delete(codigo);
+		}catch(EmptyResultDataAccessException e){
+			return ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.noContent().build();
 	}
 
 }
